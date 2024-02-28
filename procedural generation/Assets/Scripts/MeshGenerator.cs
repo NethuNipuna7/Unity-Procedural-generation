ba4c71b79e4f766ece4bdf2656cec79d871eeb05
Your Name <you@example.com>
2024-02-28 09:27:44 +0530

    commit

--- procedural generation/Assets/Scripts/MeshGenerator.cs
+++ procedural generation/Assets/Scripts/MeshGenerator.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
@@ -23,17 +25,55 @@ public class MeshGenerator : MonoBehaviour
 
     void CreateShape()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(0,0,0),
-            new Vector3(0,0,1),
-            new Vector3(1,0,0)
-        };
+        //vertices = new Vector3[]
+        //{
+        //    new Vector3(0,0,0),
+        //    new Vector3(0,0,1),
+        //    new Vector3(1,0,0),
+        //    new Vector3(1,0,1)
+        //};
 
-        triangles = new int[]
+        //triangles = new int[]
+        //{
+        //    0,1,2,
+        //    1,3,2
+        //};
+
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            0,1,2
-        };
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+
+        }
+        triangles = new int[xSize*zSize* 6];
+        int vert = 0;
+        int tris = 0;
+
+        for(int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+            }
+
+            vert++; 
+        }
+
+        
+       
     }
 
     void updatemesh()
@@ -41,5 +81,20 @@ public class MeshGenerator : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+
+       mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
     }
 }
